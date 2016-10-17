8e105794a5f22bbd9050d36b39775d3fb95c7c00
Robert Hurst <robertwhurst@gmail.com>
2016-10-17 15:24:59 -0700

    Add tests for nextTick

--- test/next-tick.spec.js
+++ test/next-tick.spec.js
@@ -2,13 +2,43 @@ const assert   = require('assert');
 const nextTick = require('../next-tick');
 
 
-describe('nextTick(fn() -> promise(result)) -> promise(result)', () => {});
+describe('nextTick(fn() -> promise(result)) -> promise(result)', () => {
+
+  it('executes the function after one tick then resolves the result resolved by the function', () => {
+    return nextTick(() => {
+      return new Promise((resolve) => {
+        resolve('value');
+      });
+    }).then((value) => {
+      assert.equal(value, 'value');
+    });
+  });
+});
 
 
-describe('nextTick(fn() -> result) -> promise(result)', () => {});
+describe('nextTick(fn() -> result) -> promise(result)', () => {
+
+  it('executes the function after one tick then resolves the result returned by the function', () => {
+    return nextTick(() => 'value').then((value) => {
+      assert.equal(value, 'value');
+    });
+  });
+});
 
 
-describe('nextTick(value) -> promise(value)', () => {});
+describe('nextTick(value) -> promise(value)', () => {
+
+  it('resolves the value after one tick', () => {
+    return nextTick('value').then((value) => {
+      assert.equal(value, 'value');
+    });
+  });
+});
 
 
-describe('nextTick() -> promise()', () => {});
+describe('nextTick() -> promise()', () => {
+
+  it('resolves after one tick', () => {
+    return nextTick();
+  });
+});
