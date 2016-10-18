cea27ccfc7791a6440ad78bdba015cf9030fdb1b
Robert Hurst <robertwhurst@gmail.com>
2016-10-17 18:00:17 -0700

    export all fns

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 exports.auto             = require('./auto');
 exports.exec             = require('./exec');
 exports.nextTick         = require('./next-tick');
-exports.parallelMapLimit = require('./parallel-map-limit');
+exports.parallelConcat   = require('./parallel-concat');
+exports.parallelFind     = require('./parallel-find');
 exports.parallelLimit    = require('./parallel-limit');
+exports.parallelMapLimit = require('./parallel-map-limit');
 exports.parallelMap      = require('./parallel-map');
 exports.parallel         = require('./parallel');
+exports.seriesConcat     = require('./series-concat');
+exports.seriesFind       = require('./series-find');
 exports.seriesMap        = require('./series-map');
 exports.series           = require('./series');
 exports.setInterval      = require('./set-interval');
